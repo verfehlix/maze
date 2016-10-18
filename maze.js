531b09d7210ef84d8b5aeb8666f89a051ac1c02f
Felix <felix.p.magnus@gmail.com>
2016-10-18 19:51:03 +0200

    basic textures added

--- maze.js
+++ maze.js
@@ -1,11 +1,14 @@
 window.onload = init;
 
+var colors = [0x1abc9c, 0x2ecc71, 0x3498db, 0x9b59b6, 0x34495e, 0x16a085, 0x27ae60, 0x2980b9, 0x8e44ad, 0x2c3e50, 0xf1c40f, 0xe67e22, 0xe74c3c, 0xecf0f1, 0x95a5a6, 0xf39c12, 0xd35400, 0xc0392b, 0xbdc3c7, 0x7f8c8d];
+
 // Cell class to hold information about specific cells
 class Cell {
-	constructor(x,y,numberOfCellsPerRow){
+	constructor(x,y,numberOfCellsPerRow,cellSize){
 		//x & y coordinates
 		this.x=x;
 		this.y=y;
+		this.size = cellSize;
 		this.maxCoordinate = numberOfCellsPerRow - 1;
 
 		//has this cell already been visited?
@@ -48,19 +51,36 @@ class Cell {
 
 		//array to hold information about the "closed" walls of this cells --> all not included walls are open
 		this.closedWalls = ["top","right","bottom","left"]
+
+		//color of the cell
+		this.color = shuffle(colors)[0];
+
+		//Sprite
+		if(Math.random() > 0.5){
+			this.sprite = new PIXI.Sprite(PIXI.loader.resources.wall1.texture);
+		} else {
+			this.sprite = new PIXI.Sprite(PIXI.loader.resources.wall2.texture);
+		}
+        // set position
+        this.sprite.position.x = x;
+        this.sprite.position.y = y;
+        // set size
+        this.sprite.width = this.size;
+        this.sprite.height = this.size;
+
 	}
 }
 
 // Grid class to hold information about the grid that holds the cells
 class Grid {
-	constructor(numberOfCellsPerRow){
+	constructor(numberOfCellsPerRow, cellSize){
 		//populate cells arary with numberOfCellsPerRow * numberOfCellsPerRow cells
 		this.cells = [];
 		this.numberOfCellsPerRow = numberOfCellsPerRow;
 
 		for (var x = 0; x < numberOfCellsPerRow; x++) {
 			for (var y = 0; y < numberOfCellsPerRow; y++) {
-				var cell = new Cell(x,y,numberOfCellsPerRow);
+				var cell = new Cell(x,y,numberOfCellsPerRow,cellSize);
 				this.cells.push(cell);
 			}
 		}
@@ -134,10 +154,10 @@ function init() {
 	//canvas size in pixels
 	var canvasSize = 750;
 	//padding from canvas border to start of grid (pixels)
-	var paddingTotal = 50;
+	var paddingTotal = 2;
 	//number of cells each row should contain (e.g. 50 --> 50x50 grid)
 	//only uneven numbers!
-	var numberOfCellsPerRow = 33;
+	var numberOfCellsPerRow = 25;
 
 	//calculate pixel size for each cell
 	var cellSize = (canvasSize - (paddingTotal)) / numberOfCellsPerRow;
@@ -161,6 +181,12 @@ function init() {
 	var last = performance.now()
 	var stop = false;
 
+	//LOAD ASSETS
+	loader
+		.add("wall1", "img/wall1.png")
+		.add("wall2", "img/wall2.png")
+		.load(setup);
+
 	//make grid globally available
 	var grid;
 
@@ -176,12 +202,6 @@ function init() {
 
 			var cell = grid.cells[i];
 
-			if(cell.visited){
-				graphics.beginFill(0x00ff99);
-			} else {
-				graphics.beginFill(0x666666);
-			}
-
 			var x = cell.x;
 			var y = cell.y;
 
@@ -189,8 +209,19 @@ function init() {
 			var drawPosX = paddingPerSide + x + ((x % numberOfCellsPerRow) * (cellSize-1));
 			var drawPosY = paddingPerSide + y + ((y % numberOfCellsPerRow) * (cellSize-1));
 
-			//draw the cell onto the canvas
-			graphics.drawRect(drawPosX, drawPosY, cellSize, cellSize);
+			if(cell.visited){
+				//draw the cell onto the canvas
+				graphics.beginFill(0x999999);
+				graphics.drawRect(drawPosX, drawPosY, cellSize, cellSize);
+			} else {
+				//graphics.beginFill(cell.color);
+				//graphics.drawRect(drawPosX, drawPosY, cellSize, cellSize);
+				cell.sprite.position.x = drawPosX;
+				cell.sprite.position.y = drawPosY;
+				stage.addChild(cell.sprite);
+			}
+
+
 		}
 		stage.addChild(graphics);
 	}
@@ -233,7 +264,7 @@ function init() {
 	function setup() {
 
 		//initialize grid
-		grid = new Grid(numberOfCellsPerRow);
+		grid = new Grid(numberOfCellsPerRow,cellSize);
 
 		// var cell = grid.getRandomCell();
 		var cell = grid.getRandomCornerCell();
@@ -267,7 +298,4 @@ function init() {
 		//render the stage
 		renderer.render(stage);
 	}
-
-	//start
-	setup();
 }
